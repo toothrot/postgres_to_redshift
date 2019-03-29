15d34236a2a42b18544a2abd88a5c6636856627c
Joshua Fleck <josh@meetcleo.com>
2019-03-29 10:24:44 +0000

    Remove rename in full import
    As full import runs in a transaction, there's really no need to replace the table using a rename. A drop and create should suffice. Truncate does not work in this case because truncate cannot be rolled back.

--- lib/postgres_to_redshift/full_import.rb
+++ lib/postgres_to_redshift/full_import.rb
@@ -11,26 +11,26 @@ module PostgresToRedshift
 
       target_connection.exec('BEGIN;')
 
-      target_connection.exec("CREATE TABLE IF NOT EXISTS #{schema}.#{target_connection.quote_ident(table.target_table_name)} (#{table.columns_for_create})")
+      # TRUNCATE cannot be rolled back
+      target_connection.exec("DROP TABLE IF EXISTS #{table_name};")
 
-      target_connection.exec("DROP TABLE IF EXISTS #{schema}.#{table.target_table_name}_updating")
+      target_connection.exec("CREATE TABLE #{table_name} (#{table.columns_for_create});")
 
-      target_connection.exec("ALTER TABLE #{schema}.#{target_connection.quote_ident(table.target_table_name)} RENAME TO #{table.target_table_name}_updating")
-
-      target_connection.exec("CREATE TABLE #{schema}.#{target_connection.quote_ident(table.target_table_name)} (#{table.columns_for_create})")
-
-      target_connection.exec("COPY #{schema}.#{target_connection.quote_ident(table.target_table_name)} FROM 's3://#{ENV['S3_DATABASE_EXPORT_BUCKET']}/export/#{table.target_table_name}.psv.gz' CREDENTIALS 'aws_access_key_id=#{ENV['S3_DATABASE_EXPORT_ID']};aws_secret_access_key=#{ENV['S3_DATABASE_EXPORT_KEY']}' GZIP TRUNCATECOLUMNS ESCAPE DELIMITER as '|';")
-
-      target_connection.exec("DROP TABLE IF EXISTS #{schema}.#{table.target_table_name}_updating")
+      target_connection.exec("COPY #{table_name)} FROM 's3://#{ENV['S3_DATABASE_EXPORT_BUCKET']}/export/#{table.target_table_name}.psv.gz' CREDENTIALS 'aws_access_key_id=#{ENV['S3_DATABASE_EXPORT_ID']};aws_secret_access_key=#{ENV['S3_DATABASE_EXPORT_KEY']}' GZIP TRUNCATECOLUMNS ESCAPE DELIMITER as '|';")
 
       if PostgresToRedshift.dry_run?
         target_connection.exec('ROLLBACK;')
       else
         target_connection.exec('COMMIT;')
+        target_connection.exec("ANALYZE #{table_name};")
       end
     end
 
     private
+
+    def table_name
+      "#{schema}.#{target_connection.quote_ident(table.target_table_name)}"
+    end
 
     attr_reader :table, :target_connection, :schema
   end
