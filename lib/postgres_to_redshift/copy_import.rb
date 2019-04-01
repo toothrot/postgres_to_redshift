f54fb774364a89d258841ede5a111be3484c797c
Joshua Fleck <josh@meetcleo.com>
2019-04-01 14:12:37 +0100

    Store the gzipped file in memory
    There's no reason to write this to a tempfile on disk if we have plenty of server memory. Should speed up the process.

--- lib/postgres_to_redshift/copy_import.rb
+++ lib/postgres_to_redshift/copy_import.rb
@@ -3,7 +3,7 @@ module PostgresToRedshift
     KILOBYTE = 1024
     MEGABYTE = KILOBYTE * 1024
     GIGABYTE = MEGABYTE * 1024
-    CHUNK_SIZE = 5 * GIGABYTE
+    CHUNK_SIZE = 2 * GIGABYTE
     BEGINNING_OF_TIME = Time.at(0).utc
 
     def initialize(table:, bucket:, source_connection:, target_connection:, schema:, incremental_from: BEGINNING_OF_TIME, incremental_to:)
@@ -34,7 +34,8 @@ module PostgresToRedshift
     end
 
     def new_tmpfile
-      tmpfile = Tempfile.new('psql2rs', encoding: 'utf-8')
+      tmpfile = StringIO.new
+      tmpfile.set_encoding('utf-8')
       tmpfile.binmode
       tmpfile
     end
@@ -45,16 +46,15 @@ module PostgresToRedshift
       [tmpfile, zip]
     end
 
-    def close_resources(tmpfile:, zip:)
+    def close_resources(zip:)
       zip.close unless zip.closed?
-      tmpfile.unlink
     end
 
     def finish_chunk(tmpfile:, zip:, chunk:)
       zip.finish
       tmpfile.rewind
       upload_table(tmpfile, chunk)
-      close_resources(tmpfile: tmpfile, zip: zip)
+      close_resources(zip: zip)
     end
 
     def copy_table
@@ -70,15 +70,15 @@ module PostgresToRedshift
             zip.write(row)
             next unless zip.pos > CHUNK_SIZE
 
-            chunk += 1
             finish_chunk(tmpfile: tmpfile, zip: zip, chunk: chunk)
+            chunk += 1
             tmpfile, zip = start_chunk
           end
         end
         finish_chunk(tmpfile: tmpfile, zip: zip, chunk: chunk)
         source_connection.reset
       ensure
-        close_resources(tmpfile: tmpfile, zip: zip)
+        close_resources(zip: zip)
       end
     end
 
