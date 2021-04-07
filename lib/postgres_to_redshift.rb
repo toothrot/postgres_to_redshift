a844ed3ecbf630eecce51cf0e8bb8a6b892cbd4a
Thomas <thomas.zimmer@jobtoday.com>
2021-04-07 20:23:23 +0200

    Update postgres_to_redshift.rb

--- lib/postgres_to_redshift.rb
+++ lib/postgres_to_redshift.rb
@@ -81,7 +81,7 @@ class PostgresToRedshift
   end
 
   def s3
-    @s3 ||= AWS::S3.new(access_key_id: ENV['S3_DATABASE_EXPORT_ID'], secret_access_key: ENV['S3_DATABASE_EXPORT_KEY'])
+    @s3 ||= Aws::S3.new(access_key_id: ENV['S3_DATABASE_EXPORT_ID'], secret_access_key: ENV['S3_DATABASE_EXPORT_KEY'])
   end
 
   def bucket
